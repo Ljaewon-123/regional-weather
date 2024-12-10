c7964cdf199110edbf17d078ae6909ecab3f961b
Ljaewon-123 <jml6388@naver.com>
2024-12-10 22:42:40 +0900

    feat: 기본 인터셉터

---
+++ weather-server/src/logging/logging.interceptor.ts
@@ -0,0 +1,32 @@
+
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, RequestTimeoutException } from '@nestjs/common';
+import { Observable, throwError, TimeoutError } from 'rxjs';
+import { catchError, tap, timeout } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Before...');
+
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+    // const { method, url, ip, query, body } = request;
+    console.log('req:', request)
+    console.log('res:', response)
+
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(
+        timeout(2000),
+        tap(() => console.log(`After... ${Date.now() - now}ms`)),
+        catchError(err => {
+          if (err instanceof TimeoutError) {
+            console.log('interceptor catchError!!!!')
+            return throwError(() => new RequestTimeoutException());
+          }
+          return throwError(() => err);
+        }),
+      );
+  }
+}
