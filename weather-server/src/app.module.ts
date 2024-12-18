9a2e4cb0104f58566ebb2f86c3b91161b0433872
Ljaewon-123 <jml6388@naver.com>
2024-12-18 15:07:32 +0900

    db연결 with local

--- weather-server/src/app.module.ts
+++ weather-server/src/app.module.ts
@@ -4,12 +4,18 @@ import { LoggingInterceptor } from './logging/logging.interceptor';
 import { WeatherModule } from './weather/weather.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConnectionService } from './connection/conected-db.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // 전체적으로 사용하기 위해
-      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true, 
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConnectionService
     }),
     ScheduleModule.forRoot(),
     WeatherModule,
---
+++ weather-server/src/connection/conected-db.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+@Injectable()
+export class TypeOrmConnectionService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>("DB_HOST"),
+      port: this.configService.get<number>("DB_PORT"),
+      username: this.configService.get<string>("DB_USERNAME"),
+      password: this.configService.get<string>("DB_PASSWORD"),
+      database: this.configService.get<string>("DB_NAME"),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: this.configService.get<boolean>("DB_SYNCHRONIZE"),
+      namingStrategy: new SnakeNamingStrategy(),
+      // logging: true,
+    }
+  }
+}
