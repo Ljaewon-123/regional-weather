02fddd777149bf318278fe33aaee1f563c60c78b
Ljaewon-123 <jml6388@naver.com>
2025-01-01 16:52:25 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# 초기 실행 시 server_green만 실행
-echo "Starting only server_green..."
-docker-compose up -d redis server_green nginx
+# 초기 실행 시 redis와 server_green만 실행
+echo "Starting only redis and server_green..."
+docker-compose up -d redis server_green
+
+# Nginx 컨테이너 시작
+echo "Starting Nginx..."
+docker-compose up -d nginx
 
 echo "Initialization completed. Active server: server_green"
