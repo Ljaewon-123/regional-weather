c4accd92dead16767a1e2f34b7b1e22f567dd7d7
Ljaewon-123 <jml6388@naver.com>
2025-01-11 18:38:00 +0900

    feat: theme selector

--- weather/app/components/ThemeSelector.vue
+++ weather/app/components/ThemeSelector.vue
@@ -1,18 +1,14 @@
 <template>
 <div>
-  <div class="p-4 pb-2 cursor-pointer flex justify-center" style="width: 100px;">
+  <div @click="clickMode = !clickMode" class="p-4 pb-2 cursor-pointer flex justify-center" style="width: 100px;">
     <span>{{ currentIcon }}</span>
 
-    <!-- <select
-      v-model="colorMode.preference"
-      class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700"
-    >
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-    </select> -->
-    <div class="absolute right-16 z-50 rounded-lg ring-1 ring-slate-900/10 shadow-lg overflow-hidden  w-36 py-1 text-sm text-slate-700 font-semibold dark:bg-gray-900 dark:ring-0 dark:highlight-white/5 dark:text-slate-300 mt-8">
-      <div v-for="mode in modes" class="py-1 px-2 flex items-center cursor-pointer text-capitalize text-sky-500">
+    <div v-if="clickMode" class="absolute right-16 z-50 rounded-lg ring-1 ring-slate-900/10 shadow-lg overflow-hidden  w-36 py-1 text-sm text-slate-700 font-semibold dark:bg-gray-900 dark:ring-0 dark:highlight-white/5 dark:text-slate-300 mt-8">
+      <div v-for="mode in modes" :key="mode" 
+        class="py-1 px-2 flex items-center cursor-pointer text-capitalize"
+        :class="mode == colorMode.preference ? 'text-sky-500' : ''"
+        @click="toggleMode(mode)"
+      >
         {{ modeIcons[mode] }}  {{ mode }}
       </div>
     </div>
@@ -21,6 +17,7 @@
 </template>
 
 <script setup lang="ts">
+const clickMode = ref(false)
 const colorMode = useColorMode()
 type ColorMode = "system" | "light" | "dark"
 const modes: ColorMode[] = [
@@ -33,15 +30,9 @@ const modeIcons = {
   light: '🌕',
   dark: '🌑'
 }
-const nextMode = computed<ColorMode>(() => {
-  const currentMode = colorMode.preference as ColorMode
-  const currentModeIndex = modes.indexOf(currentMode)
-  if(currentModeIndex >= modes.length) return "system"
 
-  return modes[currentModeIndex] ?? 'system'
-})
 const currentIcon = computed(() => modeIcons[colorMode.preference as ColorMode])
-const toggleMode = () => colorMode.preference = nextMode.value
+const toggleMode = (mode: ColorMode) => colorMode.preference = mode
 </script>
 
 <style lang="css" scoped>
