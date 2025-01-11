ef2235e5bfa273ecd6e160203f59b7b0b24f492a
Ljaewon-123 <jml6388@naver.com>
2025-01-11 20:34:43 +0900

    feat: create header

---
+++ weather/app/components/NHeader.vue
@@ -0,0 +1,13 @@
+<template>
+<header class="flex justify-between items-center shadow-lg px-8">
+  <div>{{ 'Current Weather Icon Need' }} 🌦 Regional Weather</div>
+  <div>
+    <!-- ClientOnly 이거를 안쓰고 Hydration해결하는 방법도 있는데 빌트인을 안쓸 이유가 따로있나??? -->
+    <ClientOnly>
+      <ThemeSelector />
+    </ClientOnly>
+  </div>
+</header>
+</template>
+
+<!-- /* ☁⛅⛈🌤🌥🌦🌧🌨🌩🌪🌫 */ -->
--- weather/app/layouts/default.vue
+++ weather/app/layouts/default.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="bg-white dark:bg-gray-800 dark:text-white">
-    <header class="flex justify-between items-center">
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div class="mr-24">
-        <!-- ClientOnly 이거를 안쓰고 Hydration해결하는 방법도 있는데 빌트인을 안쓸 이유가 따로있나??? -->
-        <ClientOnly>
-          <ThemeSelector />
-        </ClientOnly>
-      </div>
-    </header>
+    <NHeader />
     <slot></slot>
   </div>
 </template>
