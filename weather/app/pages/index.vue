406df5a273b72cfc830fcbff755ebd6b761e01ea
Ljaewon-123 <jml6388@naver.com>
2025-01-12 14:36:51 +0900

    style: postcss

--- weather/app/pages/index.vue
+++ weather/app/pages/index.vue
@@ -31,7 +31,7 @@
 
 </script>
 
-<style scoped>
+<style scoped lang="postcss">
 .badge {
   @apply inline-block bg-gray-200 dark:bg-gray-950 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-400;
   &:hover {
