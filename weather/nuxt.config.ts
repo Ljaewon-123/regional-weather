187f32a7d0faffec9a128a5dd05d14101635cc65
Ljaewon-123 <jml6388@naver.com>
2025-01-15 15:52:29 +0900

    feat: download shadcn-nuxt
    need just chart components

--- weather/nuxt.config.ts
+++ weather/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'shadcn-nuxt'],
   colorMode: {
     classSuffix: ''
   },
