e7e42c3833f6908455720d8d44d4e865aacb98c5
Ljaewon-123 <jml6388@naver.com>
2025-01-15 19:51:41 +0900

    feat: wrap select

---
+++ weather/app/components/NSelect/index.vue
@@ -0,0 +1,44 @@
+<!-- slot를 삭제하고 props로 items을 채우느냐 slot으로 남겨두는냐.. -->
+<!-- 매번 전자를 사용했으니 이번엔 후자로 한번 사용해봄 -->
+<template>
+  <Select v-model="select">
+    <SelectTrigger class="text-capitalize" :class="`w-[${props.width}px]`">
+      <SelectValue :placeholder="props.placeholder" />
+    </SelectTrigger>
+    <SelectContent>
+      <slot>
+        <SelectGroup>
+          <SelectLabel>Items</SelectLabel>
+          <SelectItem 
+            v-for="item in defaultItems" 
+            :key="item" 
+            :value="item" 
+            class="text-capitalize"
+          >
+            {{ item }}
+          </SelectItem>
+        </SelectGroup>
+      </slot>
+    </SelectContent>
+  </Select>
+</template>
+
+<script setup lang="ts">
+// um... generic script?
+const select = defineModel({ default: undefined })
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: "Select a Item"
+  },
+  width: {
+    type: Number,
+    default: 80
+  }
+})
+const defaultItems = [
+  'defulat1',
+  'defulat2',
+  'defulat3',
+]
+</script>
