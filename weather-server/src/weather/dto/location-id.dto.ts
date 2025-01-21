3944742838894164b5b64f42fce44b008917653f
Ljaewon-123 <jml6388@naver.com>
2025-01-21 22:20:21 +0900

    fix: LocationIds dto수정

--- weather-server/src/weather/dto/location-id.dto.ts
+++ weather-server/src/weather/dto/location-id.dto.ts
@@ -1,6 +1,8 @@
+import { Transform, Type } from 'class-transformer';
 import { IsArray, IsInt, ArrayNotEmpty, ArrayMinSize } from 'class-validator';
 
 export class LocationIdsDto {
+  @Transform(({ value }) => JSON.parse(value))
   @IsArray()
   @ArrayNotEmpty()
   @ArrayMinSize(1)
