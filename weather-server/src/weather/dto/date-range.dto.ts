dfb065d7df698f3d5851f16e78eb85b880bb0eaf
Ljaewon-123 <jml6388@naver.com>
2025-01-23 21:24:14 +0900

    update: dto messages

--- weather-server/src/weather/dto/date-range.dto.ts
+++ weather-server/src/weather/dto/date-range.dto.ts
@@ -3,20 +3,20 @@ import { LocationIdDto } from './location-id.dto';
 
 export class DateRangeDto {
   @IsString()
-  @Matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, { message: 'startDate must be in the format yyyy-MM-dd HH' })
+  @Matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, { message: 'startDate must be in the format yyyy-MM-dd HH:mm:ss' })
   startDate: string;
 
   @IsString()
-  @Matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, { message: 'endDate must be in the format yyyy-MM-dd HH' })
+  @Matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, { message: 'endDate must be in the format yyyy-MM-dd HH:mm:ss' })
   endDate: string;
 }
 
 export class DateRangeDtoWithLocationId extends LocationIdDto {
   @IsString()
-  @Matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, { message: 'startDate must be in the format yyyy-MM-dd HH' })
+  @Matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, { message: 'startDate must be in the format yyyy-MM-dd HH:mm:ss' })
   startDate: string;
 
   @IsString()
-  @Matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, { message: 'endDate must be in the format yyyy-MM-dd HH' })
+  @Matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, { message: 'endDate must be in the format yyyy-MM-dd HH:mm:ss' })
   endDate: string;
 }
