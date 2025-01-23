3de07a86a8774bc610358844dc934c1e78cd08d4
Ljaewon-123 <jml6388@naver.com>
2025-01-23 22:34:52 +0900

    desigon: color popup

--- weather/app/components/NHeader.vue
+++ weather/app/components/NHeader.vue
@@ -14,9 +14,20 @@
             <Icon name="mdi:brush-variant"  />
           </Button>
         </PopoverTrigger>
-        <PopoverContent class="w-80">
-          <div>
-            <div class="w-5 h-5 rounded-full bg-red-600"></div>
+        <PopoverContent style="width: 400px;">
+          <div class="p-4">
+            <div class="mb-8">
+              <h1 class="text-md text-foreground font-semibold">Customize</h1>
+              <p class="text-xs text-muted-foreground">Pick up the your color</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 py-1.5" >
+              <Button v-for="color in colors" variant="outline" class="inline-flex items-center gap-2 whitespace-nowrap px-3 shadow-sm hover:bg-accent hover:text-accent-foreground py-2 h-8 justify-start px-3">
+                <span class="w-5 h-5 rounded-full" :class="color.class"></span> 
+                <span class="text-xs capitalize">
+                  {{ color.title }}
+                </span>
+              </Button>
+            </div>
           </div>
         </PopoverContent>
       </Popover>
@@ -33,4 +44,17 @@
 
 <script setup lang="ts">
 const localePath = useLocalePath()
+
+const colors = [
+  { title: "zinc" ,class: "bg-zinc-500" },
+  { title: "rose" ,class: "bg-rose-500" },
+  { title: "blue" ,class: "bg-blue-500" },
+  { title: "green" ,class: "bg-green-500" },
+  { title: "orange" ,class: "bg-orange-500" },
+  { title: "red" ,class: "bg-red-500" },
+  { title: "gray" ,class: "bg-gray-500" },
+  { title: "yellow" ,class: "bg-yellow-500" },
+  { title: "violet" ,class: "bg-violet-500" },
+]
+
 </script>
